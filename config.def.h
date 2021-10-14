e5139ae57c1cf1a5ff8a0de78eaaca455a699921
thehnm <thehnm@posteo.net>
2021-10-14 17:27:43 +0200

    New default color scheme

--- config.def.h
+++ config.def.h
@@ -96,29 +96,29 @@ unsigned int tabspaces = 8;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-    "#282a2e",
-    "#a54242",
-    "#8c9440",
-    "#de935f",
-    "#5f819d",
-    "#85678f",
-    "#5e8d87",
-    "#707880",
+    "#282828",
+    "#cc241d",
+    "#98971a",
+    "#d79921",
+    "#458588",
+    "#b16286",
+    "#689d6a",
+    "#a89984",
     /* 8 bright colors */
-    "#373b41",
-    "#cc6666",
-    "#b5bd68",
-    "#f0c674",
-    "#81a2be",
-    "#b294bb",
-    "#8abeb7",
-    "#c5c8c6",
+    "#928374",
+    "#fb4934",
+    "#b8bb26",
+    "#fabd2f",
+    "#83a598",
+    "#d3869b",
+    "#8ec07c",
+    "#ebdbb2",
 
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
     "#282828",
-    "#c5c8c6",
+    "#ebdbb2",
     "#707880",
 };
 
